f321386532ef3c4fb76fe1e1eb1007631ee98f9c
jcas12 <jarturocs0412@gmail.com>
2025-07-04 18:37:43 -0400

    Load .env token, initialize Discord client with message content intent

--- groovetube.py
+++ groovetube.py
@@ -3,3 +3,11 @@ import os
 import asyncio
 import yt_dlp
 from dotenv import load_dotenv
+
+def run_bot():
+    load_dotenv()
+    TOKEN = os.getenv('discord_token')
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents)
+    
